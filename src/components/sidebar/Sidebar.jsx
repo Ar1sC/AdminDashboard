f9ea8c6ebdd8fbabd6567b1b977839bfe308a42b
Aris <Ar1sC@users.noreply.github.com>
2024-03-31 22:06:59 +0300

    Sidebar Changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -15,13 +16,34 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <Person2OutlinedIcon />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <Inventory2OutlinedIcon />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <span>Orders</span>
+          </li>
+          <li>
+            <span>Notifications</span>
+          </li>
+          <li>
+            <span>System health</span>
+          </li>
+
+          <li>
+            <span>Logs</span>
+          </li>
+          <li>
+            <span>Settings</span>
+          </li>
+          <li>
+            <span>Profile</span>
+          </li>
+          <li>
+            <span>Logout</span>
           </li>
         </ul>
       </div>
